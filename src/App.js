3556497c5b00fb151956efb20eaafd63ccd221ac
Aditya_Pathak <buddingwebdeveloperind@gmail.com>
2019-09-03 07:38:23 +0530

    Adding face detection API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+
+import Clarifai from 'clarifai';
+
+const app= new Clarifai.App({
+  apiKey: '6ba473f99dd346b9ac4cfd50ffd6b614'
+})
 
 
 const particlesOptions ={
@@ -21,7 +28,57 @@ const particlesOptions ={
 
 
 
-function App() {
+class App extends Component {
+ constructor(){
+  super();
+  this.state={
+    input:'',
+    imageUrl:''
+  }
+ } 
+ 
+ onInputChange=(event)=>{
+  this.setState({input:event.target.value});
+ }
+
+ onButtonSubmit=()=>{
+  
+   this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+       .then(
+        function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        },
+        function(err){
+          
+        }
+      )
+     }
+
+
+
+      //if (response) {
+      //     fetch('http://localhost:3000/image', {
+      //       method: 'put',
+      //       headers: {'Content-Type': 'application/json'},
+      //       body: JSON.stringify({
+      //         id: this.state.user.id
+      //       })
+      //     })
+      //       .then(response => response.json())
+      //       .then(count => {
+      //         this.setState(Object.assign(this.state.user, { entries: count}))
+      //       })
+
+      //   }
+      //   this.displayFaceBox(this.calculateFaceLocation(response))
+      // })
+      // .catch(err => console.log(err));
+  
+render(){
   return (
    
     <div className="App">
@@ -30,10 +87,12 @@ function App() {
      <Navigation />
      <Logo />
      <Rank />
-     <ImageLinkForm />
-     
+     <ImageLinkForm
+      onInputChange={this.onInputChange}
+      onButtonSubmit={this.onButtonSubmit}/>
+     <FaceRecognition  imageUrl={this.state.imageUrl}/>
     </div>
   );
+ }
 }
-
 export default App;
